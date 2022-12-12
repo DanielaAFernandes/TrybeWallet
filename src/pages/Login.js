e03dcf6cac033c807e303c5aa7ff69bc71e1a9b0
Daniela <daniela@pop-os.localdomain>
2022-12-12 15:31:32 -0300

    Ver o erro do cypress

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,96 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import '../index.css';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { addEmailAndPassword } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    emailInput: '',
+    passwordInput: '',
+    isSubmitButtonDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.setState({
+        isSubmitButtonDisabled: !this.validateEmailPassword(),
+      });
+    });
+  };
+
+  validateEmailPassword = () => {
+    const { emailInput, passwordInput } = this.state;
+    const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/i;
+    const email = emailRegex.test(emailInput);
+    const minNumber = 6;
+    return passwordInput.length >= minNumber && email;
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { dispatch } = this.props;
+    dispatch(addEmailAndPassword(this.state));
+  };
+
   render() {
-    return <div>Login</div>;
+    const { emailInput, isSubmitButtonDisabled, passwordInput } = this.state;
+    return (
+      <div className="login-container">
+        {/* <div className="trybe-logo">
+          <img
+            src="https://theme.zdassets.com/theme_assets/9633455/ecf228e8c15da1a8bd07f574e675a0ac59330968.png"
+            alt="trybe-logo"
+          />
+        </div> */}
+        <form className="form-container" onSubmit={ this.handleSubmit }>
+          <label className="login-label" htmlFor="login-input">
+            Login:
+            <input
+              className="login"
+              type="email"
+              data-testid="email-input"
+              name="login-input"
+              id="login-input"
+              onChange={ this.handleChange }
+              value={ emailInput }
+            />
+          </label>
+          <label className="senha-label" htmlFor="senha-input">
+            Senha:
+            <input
+              className="senha"
+              type="text"
+              data-testid="password-input"
+              name="senha-input"
+              id="senha-input"
+              onChange={ this.handleChange }
+              value={ passwordInput }
+            />
+          </label>
+          <div className="entrar-button">
+            <Link to="/carteira">
+              <button
+                className="submit-button"
+                type="submit"
+                disabled={ isSubmitButtonDisabled }
+              >
+                Entrar
+              </button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,10 @@
-// Coloque aqui suas actions
+// Coloque aqui suas actions\
+export const ADD_LOGIN = 'ADD_LOGIN';
+
+export const addEmailAndPassword = (personalInfo) => {
+  console.log('pelo amor de Deus');
+  return {
+    type: ADD_LOGIN,
+    payload: { ...personalInfo },
+  };
+};
