ffc21b1c46461b9218d8cfbcd95c426549cf268a
Daniela <daniela@pop-os.localdomain>
2022-12-15 09:27:19 -0300

    Requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
+    const tableData = expenses.map(({
+      id,
+      value,
+      description,
+      currency,
+      method,
+      tag,
+      exchangeRates,
+    }) => (
+      <tr key={ id }>
+        <td>{ description }</td>
+        <td>{ tag }</td>
+        <td>{ method }</td>
+        <td>{ Number(value).toFixed(2)}</td>
+        <td>{ exchangeRates[currency].name }</td>
+        <td>{ Number(exchangeRates[currency].ask).toFixed(2)}</td>
+        <td>{ Number(exchangeRates[currency].ask * value).toFixed(2)}</td>
+        <td>Real</td>
+      </tr>
+    ));
     return (
       <div>
         <table className="table-edit">
           <thead>
             <tr>
-              <th className="description-table">Descrição</th>
+              <th className="description-table"> Descrição </th>
               <th className="tag-table">Tag</th>
               <th className="method-table">Método de pagamento</th>
               <th className="value-table">Valor</th>
@@ -18,10 +41,21 @@ class Table extends Component {
               <th className="edit-delete-table">Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {tableData}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.instanceOf(Array).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -11,6 +11,7 @@ class WalletForm extends Component {
     method: 'Dinheiro',
     tag: 'Alimentação',
     description: '',
+    exchangeRates: '',
   };
 
   componentDidMount() {
